c441ca048378978199aa0fdd30037f1f74f82f05
KharshavReddy <87965902+KharshavReddy@users.noreply.github.com>
2021-10-05 13:28:13 +0530

    Update database.py

--- todo_app/database.py
+++ todo_app/database.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:5809@127.0.0.1:5432/postgres"
 
 
